e44ab784a7260f2eb5a4353d57852732091ed51c
dc <1090504489@qq.com>
2025-04-21 15:54:21 +0800

    第一版

---
+++ src/test/java/top/dc/javaservice/JavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.dc.javaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
